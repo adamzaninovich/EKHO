92e526d4b199642d5b07996ff481a1ea0d2873f3
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2014-01-03 17:28:02 -0800

    updates

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -5,53 +5,54 @@ class DevicesController < ApplicationController
   end
 
   def show
-    @device = device_for_id params[:id]
+    get_device
   end
 
   def now_playing
-    @device = device_for_id params[:id]
+    get_device
+    render 'now_playing', format: :js
   end
 
   # controls
 
   def pause
-    device = device_for_id params[:id]
-    device.pause
+    get_device
+    @device.pause
   end
 
   def play
-    device = device_for_id params[:id]
-    device.play
+    get_device
+    @device.play
   end
 
   def next
-    device = device_for_id params[:id]
-    device.next
+    get_device
+    @device.next
   end
 
   def previous
-    device = device_for_id params[:id]
-    device.previous
+    get_device
+    @device.previous
   end
 
   def vol_up
-    device = device_for_id params[:id]
-    device.volume += 10
+    get_device
+    @device.volume += 10
   end
 
   def vol_down
-    device = device_for_id params[:id]
-    if device.volume > 10
-      device.volume -= 10
+    get_device
+    if @device.volume > 10
+      @device.volume -= 10
     else
-      device.volume = 1
+      @device.volume = 0
     end
   end
 
   private
 
-  def device_for_id device_id
-    sonos.devices.select do |device|
+  def get_device device_id=params[:id]
+    @device = sonos.devices.select do |device|
       device.name.parameterize == device_id
     end.first
   end
