32baca9c1969e1aae46cecfc1da2ad5767bcc42e
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2014-01-05 23:25:43 -0800

    fixes a bug

--- app/helpers/devices_helper.rb
+++ app/helpers/devices_helper.rb
@@ -3,12 +3,14 @@ module DevicesHelper
     hours, minutes, seconds = *timecode.split(':').map(&:to_i)
     hours*60*60 + minutes*60 + seconds
   end
-  def format_time timecode
+  def format_time timecode, minimum_seconds=30
     hours, minutes, seconds = *timecode.split(':').map(&:to_i)
     if hours > 0
       timecode
-    elsif seconds > 30
+    elsif seconds > minimum_seconds || minutes > 0
       "#{minutes}:#{seconds.to_s.rjust 2, '0'}"
+    else
+      "#{seconds}"
     end
   end
   def percent_played now_playing
