a972d0ca7ed87b3eeb6634ce8393777ffff77e48
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2014-01-20 07:41:53 -0800

    removes 'send to glass' button

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -14,11 +14,6 @@ class DevicesController < ApplicationController
     @device = sonos.get_device params[:id]
   end
 
-  def glass_update_track
-    GlassUpdateTrackWorker.perform_async current_user.id, params[:id]
-    render nothing: true
-  end
-
   # controls
 
   def pause
