3a4802f179bad9b7b49b735993e60a4fb81caef1
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2014-02-09 09:55:59 -0800

    updates spark worker

--- app/workers/spark_update_track_worker.rb
+++ app/workers/spark_update_track_worker.rb
@@ -8,7 +8,7 @@ class SparkUpdateTrackWorker
   sidekiq_options retry: false
 
   recurrence do
-    minutely.second_of_minute 0, 10, 20, 30, 40, 50
+    minutely.second_of_minute 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55
   end
 
   def device_id
@@ -18,8 +18,11 @@ class SparkUpdateTrackWorker
   def perform
     User.where(provider: 'spark').each do |user|
       track = TrackUpdate.find_or_init_with user_id: user.id, device_id: device_id
-      unless track.persisted?
-        track.persisted? ? track.touch : track.save
+      if track.persisted?
+        Rails.logger.info "Not sending #{track.title} by #{track.artist}."
+      else
+        Rails.logger.info "Sending #{track.title} by #{track.artist}."
+        track.save
         update_spark! user.uid, track.title, track.artist
       end
     end
@@ -29,7 +32,8 @@ class SparkUpdateTrackWorker
     if device_id
       message = format_for_lcd artist, title
       Rails.logger.info "Sending Message to Spark: #{message}"
-      SparkMessageSender.new(device_id).send message
+      response = SparkMessageSender.new(device_id).send message
+      Rails.logger.info response.inspect
       Rails.logger.info "DONE"
     else
       Rails.logger.error "No Device ID!"
